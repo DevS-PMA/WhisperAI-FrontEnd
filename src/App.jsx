bf3222225721dcc66ce0c8aa6a755a1fdc85f29b
Lalith aditya Chunduri <chun8135@vandals.uidaho.edu>
2025-08-03 12:13:34 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import Navbar from './sections/Navbar'
 import HeroSection from './sections/HeroSection'
 import WhisperInput from './sections/WhisperInput'
+import SafePopup from './sections/SafePopup'
 
 export default function App() {
   return (
     <div className="bg-[#fefcfc] text-[#4a2f2f] font-sans">
       <Navbar />
+      <SafePopup />
       <HeroSection />
       <WhisperInput />
     </div>
