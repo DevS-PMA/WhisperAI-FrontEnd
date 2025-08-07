a03ca670026c5cf55edfbc21004f2acceb2010f1
Lalith aditya Chunduri <chun8135@vandals.uidaho.edu>
2025-08-07 17:27:00 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './index.css'
 import App from './App'
 import ChatPage from './pages/ChatPage'
-import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename="/whisper-ai">
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/chat" element={<ChatPage />} />
