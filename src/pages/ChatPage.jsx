a11cf55c4d498be64cc1d6006aa638a210837129
Nightyelf2403 <chun8135@vandals.uidaho.edu>
2025-08-29 12:05:04 -0700

    cleared the issue of usecontext

--- src/pages/ChatPage.jsx
+++ src/pages/ChatPage.jsx
@@ -4,7 +4,7 @@ import { Mic } from 'lucide-react'
 import { Link, NavLink, useLocation } from 'react-router-dom'
 import { ChevronDown, ChevronRight, PlusSquare, History, NotebookPen, Shield, ExternalLink } from 'lucide-react'
 import api from '../scripts/api' 
-import { LoginContext } from '../App'
+import { LoginContext } from '../App.jsx'
 
 
 export default function ChatPage() {
