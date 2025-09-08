b1fcbe42cc34528e0d8cff863910b39fddb78c96
DevS-PMA <Sharma.diwakar2009@gmail.com>
2025-09-07 21:03:17 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Replace below with your repo name!
 export default defineConfig({
   plugins: [react()],
-  base: '/whisper-ai/', // <-- your GitHub repo name
+  base: '/',                 // ← important: NO '/whisper-ai/'
 })
