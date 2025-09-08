5a92ed602b98b4a2de054a506a812e5a4042fa3e
DevS-PMA <Sharma.diwakar2009@gmail.com>
2025-09-07 21:27:37 -0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/',                 // ← important: NO '/whisper-ai/'
+  base: '/whisper-ai/',                 // ← important: NO '/whisper-ai/'
 })
