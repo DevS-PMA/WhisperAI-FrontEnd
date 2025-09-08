edcb0c1785891f73ad824112c7b04a72dd392758
DevS-PMA <Sharma.diwakar2009@gmail.com>
2025-09-07 21:17:43 -0700

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import App from './App'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter basename="/whisper-ai">
+    <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
   </React.StrictMode>
